b0ea20b32bacffcfef0cf40bff83da835ee51f25
dnew0130 <DavidN988878@gmail.com>
2022-11-16 18:38:34 -0800

    init - initialize functional server

---
+++ server.js
@@ -0,0 +1,20 @@
+//requiring express; creating server
+const express = require('express');
+const app = express();
+
+const port = 8000; //just keeping it simple
+
+//Dependencies
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+//Initialize project folder
+app.use(express.static('website'));
+
+//listen method with arrow function
+const server = app.listen(port, () => {
+    console.log(`Server is listening on localhost: ${port}`);
+});
